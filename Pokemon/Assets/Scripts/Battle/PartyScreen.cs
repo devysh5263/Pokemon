52121514463442717bd234cc1d4ff50bafd4d89d
devysh5263 <devysh5263@gmail.com>
2022-02-14 00:55:51 +0900

    #16

--- Pokemon/Assets/Scripts/Battle/PartyScreen.cs
+++ Pokemon/Assets/Scripts/Battle/PartyScreen.cs
@@ -6,7 +6,9 @@ using UnityEngine.UI;
 public class PartyScreen : MonoBehaviour
 {
     [SerializeField] Text messageText;
+
     PartyMemberUI[] memberSlots;
+    List<Pokemon> pokemons;
 
     public void Init()
     {
@@ -15,6 +17,8 @@ public class PartyScreen : MonoBehaviour
 
     public void SetPartyData(List<Pokemon> pokemons)
     {
+        this.pokemons = pokemons;
+
         for (int i= 0; i<memberSlots.Length; i++)
         {
             if (i < pokemons.Count)
@@ -26,4 +30,19 @@ public class PartyScreen : MonoBehaviour
         messageText.text = "Choose a Pokemon";
     }
 
+    public void UpdateMemberSelection(int selectedMember)
+    {
+        for(int i = 0; i< pokemons.Count; i++)
+        {
+            if (i == selectedMember)
+                memberSlots[i].SetSelected(true);
+            else
+                memberSlots[i].SetSelected(false);
+        }
+    }
+
+    public void SetMessageText(string message)
+    {
+        messageText.text = message;
+    }
 }
