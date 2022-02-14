52b21c832730fc6d4c3736d41a9707e472cafc25
devysh5263 <devysh5263@gmail.com>
2022-02-14 15:16:32 +0900

    #18

--- Pokemon/Assets/Scripts/Pokemons/MoveBase.cs
+++ Pokemon/Assets/Scripts/Pokemons/MoveBase.cs
@@ -14,6 +14,9 @@ public class MoveBase : ScriptableObject
     [SerializeField] int power;
     [SerializeField] int accuracy;
     [SerializeField] int pp;
+    [SerializeField] MoveCategory category;
+    [SerializeField] MoveEffects effects;
+    [SerializeField] MoveTarget target;
 
     public string Name { get { return name; } }
     public string Description { get { return description; } }
@@ -21,16 +24,32 @@ public class MoveBase : ScriptableObject
     public int Power { get { return power; } }
     public int Accuracy { get { return accuracy; } }
     public int PP { get { return pp; } }
-
-    public bool IsSpecial { get {
-            if (type == PokemonType.Fire || type == PokemonType.Water || type == PokemonType.Grass
-                || type == PokemonType.Ice || type == PokemonType.Electric || type == PokemonType.Dragon)
-            {
-                return true;
-            }
-            else
-                return false;
-        } 
-    }
+    public MoveCategory Category { get { return category; } }
+    public MoveEffects Effects { get { return effects; } }
+    public MoveTarget Target { get { return target; } }
     
+}
+
+[System.Serializable]
+public class MoveEffects
+{
+    [SerializeField] List<StatBoost> boosts;
+    public List<StatBoost> Boosts { get { return boosts; } }
+}
+
+[System.Serializable]
+public class StatBoost
+{
+    public Stat stat;
+    public int boost;
+}
+
+public enum MoveCategory
+{
+    Physical, Special, Status
+}
+
+public enum MoveTarget
+{
+    Foe, Self
 }
--- Pokemon/Assets/Scripts/Pokemons/PokemonBase.cs
+++ Pokemon/Assets/Scripts/Pokemons/PokemonBase.cs
@@ -70,6 +70,14 @@ public enum PokemonType
     Dragon
 }
 
+public enum Stat
+{
+    Attack,
+    Defense,
+    SpAttack,
+    SpDefense,
+    Speed
+}
 public class TypeChart
 {
     static float[][] chart =
